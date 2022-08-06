288b135b9ade7720acd67863a2cd2f8d5096a794
Mukesh Goyal <mukesh.goyal@theaa.com>
2022-08-06 07:32:28 +0100

    etes

--- src/app/admin/components/posts/create/create.component.ts
+++ src/app/admin/components/posts/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostService } from 'src/app/admin/services/post.service';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public postService : PostService,
+    private router: Router
+  ) { }
+
+  title:string = ""
+  content:string = ""
 
   ngOnInit(): void {
+  }
+
+  create(){
+
+    let id = 1;
+    this.postService.posts.map((post)=>{
+      if(post.id > id){
+        id = post.id;
+      }
+    })
+    id++;
+
+    this.postService.posts.push({
+      id,
+      title : this.title,
+      content : this.content,
+      date:"31-06-2021"
+    })
+
+    console.log(this.postService.posts)
+
+    this.router.navigate(['posts']);
   }
 
 }
--- src/app/admin/components/posts/edit/edit.component.ts
+++ src/app/admin/components/posts/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { PostService } from 'src/app/admin/services/post.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  postId: number = 0;
+  postdata:any = {};
+  title:string = "";
+  content:string = "";
+  constructor(
+    public postService : PostService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { 
+    this.route.params.subscribe(params => {
+      this.postId = +params['postId'];
+      this.postdata = this.postService.posts.find((post)=>{
+        if(post.id == this.postId){
+          return true
+        }
+        return false
+      })
+
+      this.title = this.postdata.title;
+      this.content = this.postdata.content;
+
+    });
+  }
 
   ngOnInit(): void {
   }
+
+  edit(){
+
+    this.postService.posts.find((post)=>{
+        if(post.id == this.postId){
+          post.title = this.title;
+          post.content = this.content;
+          post.date = "05-08-2022";
+          return true
+        }
+        return false
+    })
+
+    this.router.navigate(['posts']);
+
+  }
+
+
+
 
 }
